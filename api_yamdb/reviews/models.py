b05673194051972beae42a2b107c334d5e28e8d2
Nikolai Orlov <nicolasorlov174@yandex.com>
2022-08-23 14:07:35 +0300

    models Categories, Genres, Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,34 @@
+from tkinter import CASCADE
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=200)
+    category_slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Genres(models.Model):
+    genre_name = models.CharField(max_length=200)
+    genre_slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Titles(models.Model):
+    title_name = models.TextField()
+    category = models.ForeignKey(
+        Categories, on_delete=models.CASCADE,
+        related_name='titles', blank=False, null=False
+    )
+    genre = models.ForeignKey(
+        Genres, on_delete=CASCADE,
+        related_name='titles', blank=False, null=False
+    )
+
+    def __str__(self):
+        return self.name
