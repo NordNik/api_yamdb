6752a989d94312cebeb343f08fafad088ff7e8c3
Nikolai Orlov <nicolasorlov174@yandex.com>
2022-08-23 14:28:56 +0300

    models Categories, Genres, Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,22 @@
 from tkinter import CASCADE
-from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import TextField, CharField, EmailField
+
+ROLE_CHOICES = [
+    ('US', 'user'),
+    ('MD', 'moderator'),
+    ('AD', 'admin'),
+]
+
+
+class User(AbstractUser):
+    bio = TextField('Биография', blank=True)
+    role = CharField(
+        max_length=2, verbose_name='Роль', choices=ROLE_CHOICES, default='US'
+    )
+    password = None
+    email = EmailField()
 
 
 class Categories(models.Model):
