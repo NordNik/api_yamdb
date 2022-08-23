d63526bc6c458ae549c57f147baee252347dc836
Nikolai Orlov <nicolasorlov174@yandex.com>
2022-08-23 16:06:25 +0300

    fixed

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import TextField, CharField, EmailField
+from django.db import models
+
 
 ROLE_CHOICES = [
     ('US', 'user'),
@@ -9,33 +10,12 @@ ROLE_CHOICES = [
 
 
 class User(AbstractUser):
-    bio = TextField(verbose_name='Biography', blank=True)
-    role = CharField(
+    bio = models.TextField(verbose_name='Biography', blank=True)
+    role = models.CharField(
         max_length=2, verbose_name='Role', choices=ROLE_CHOICES, default='US'
     )
     password = None
-    email = EmailField()
-<<<<<<< HEAD
-=======
->>>>>>> refs/remotes/origin/master
-from django.contrib.auth.models import AbstractUser
-from django.db.models import TextField, CharField, EmailField
-
-ROLE_CHOICES = [
-    ('US', 'user'),
-    ('MD', 'moderator'),
-    ('AD', 'admin'),
-]
-
-
-class User(AbstractUser):
-<<<<<<< HEAD
-    bio = TextField('Биография', blank=True)
-    role = CharField(
-        max_length=2, verbose_name='Роль', choices=ROLE_CHOICES, default='US'
-    )
-    password = None
-    email = EmailField()
+    email = models.EmailField()
 
 
 class Categories(models.Model):
@@ -61,17 +41,9 @@ class Titles(models.Model):
         related_name='titles', blank=False, null=False
     )
     genre = models.ForeignKey(
-        Genres, on_delete=CASCADE,
+        Genres, on_delete=models.CASCADE,
         related_name='titles', blank=False, null=False
     )
 
     def __str__(self):
         return self.name
-=======
-    bio = TextField(verbose_name='Biography', blank=True)
-    role = CharField(
-        max_length=2, verbose_name='Role', choices=ROLE_CHOICES, default='US'
-    )
-    password = None
-    email = EmailField()
->>>>>>> refs/remotes/origin/master
