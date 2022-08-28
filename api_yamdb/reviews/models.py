3a0d835260e378922f708b11bba1b97234721d15
yllen32 <furtov1995@yandex.ru>
2022-08-28 12:38:07 +0300

    fix UserModel

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -12,17 +12,11 @@ ROLE_CHOICES = [
 class User(AbstractUser):
     bio = models.TextField(verbose_name='Biography', blank=True)
     role = models.CharField(
-        max_length=9, verbose_name='Role',
-        choices=ROLE_CHOICES, default='user'
+        max_length=9, verbose_name='Role', choices=ROLE_CHOICES, default='user'
     )
     password = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(unique=True)
-
-
-class ConfirmationData(models.Model):
-    confirmation_email = models.EmailField()
-    confirmation_username = models.CharField(max_length=19)
-    confirmation_code = models.CharField(max_length=8, )
+    confirmation_code = models.CharField(max_length=8,)
 
 
 class Categorie(models.Model):
